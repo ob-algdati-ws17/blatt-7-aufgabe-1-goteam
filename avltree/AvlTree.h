e05e51d1393d8edb8b045ec2cfd05a89af86e53f
Korbinian Karl <korbi@fs.cs.hm.edu>
2018-01-14 05:33:01 +0100

    Doxygen testing

--- avltree/AvlTree.h
+++ avltree/AvlTree.h
@@ -1,6 +1,19 @@
 #ifndef AVLTREE_LIBRARY_H
 #define AVLTREE_LIBRARY_H
 
+/*! \mainpage My Personal Index Page
+ *
+ * \section intro_sec Introduction
+ *
+ * This is the introduction.
+ *
+ * \section install_sec Installation
+ *
+ * \subsection step1 Step 1: Opening the box
+ *
+ * etc...
+ */
+
 /**
  * @file AvlTree.h
  * @class AvlTree.cpp
